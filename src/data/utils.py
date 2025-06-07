4f927ad66e2877c6897e823e585d41c5c0e08050
Arthur Zakirov <82472338+ArthurZakirov@users.noreply.github.com>
2025-06-07 18:30:12 +0000

    Remove unused email name preprocessing function from utils.py

--- src/data/utils.py
+++
@@ -1,6 +0,0 @@
-def email_name_preprocessing(df):
-    pattern = r'(?P<Name>.*?)(?:\s+<)?(?P<Email>[\w\.-]+@[\w\.-]+)>?'
-    df[['Name', 'Email']] = df['From'].str.extract(pattern)
-    df.replace('', pd.NA, inplace=True)
-    df[['Name', 'Email']] 
-    return df
